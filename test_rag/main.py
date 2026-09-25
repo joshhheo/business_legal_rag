f1d96e3935462dd0559de291cde2d57fa837ac0a
Joshua Heo <jhheo1@uci.edu>
2026-09-24 22:20:10 -0700

    give chunks to gemini llm

--- test_rag/main.py
+++ test_rag/main.py
@@ -1,6 +1,8 @@
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
-
+import os
+from dotenv import load_dotenv
+from google import genai
 
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
@@ -26,18 +28,42 @@ question = "How can a contract be terminated?"
 # encode expects 2D array
 question_embedding = model.encode([question])
 
+# returns 2D list
 similarities = cosine_similarity(question_embedding, embeddings)
-
-print(similarities)
-
-similarities = similarities[0]
 
 # arg sort is ascending by default
 # arg sort saves initial index positions of each value
 # index position represents chunk identity
-# 2 slices because reverse order before indexing
-sorted_similarities = similarities.argsort()[::-1][:2]
+sorted_similarities = similarities[0].argsort()[::-1]
 
-for index in sorted_similarities:
-    print(all_chunks[index])
+top_matches = sorted_similarities[:2]
 
+retrieved_chunks = []
+
+for index in top_matches:
+    retrieved_chunks.append(all_chunks[index])
+
+load_dotenv()
+
+api_key = os.getenv("gemini_API_key")
+
+client = genai.Client(api_key=api_key)
+
+context = retrieved_chunks
+
+prompt = f"""
+Answer the question using only the provided context.
+
+Context:
+{context}
+
+Question:
+{question}
+"""
+
+response = client.models.generate_content(
+    model="gemini-3.5-flash-lite",
+    contents=prompt
+)
+
+print(response.text)
